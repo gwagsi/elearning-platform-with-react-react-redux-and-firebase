b68f7bc914ef865d9b4ddfdbe84be0e91ea65680
gwagsi <gwagsiglenn@gmail.com>
2020-06-14 14:23:38 +0100

    changing links that apear in the side bar

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@
 import Index from "views/Index.js";
 import Profile from "views/examples/Profile.js";
 import Maps from "views/examples/Maps.js";
-import Register from "views/examples/Register.js";
+//simport Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
@@ -17,44 +17,37 @@ var routes = [
   },
   {
     path: "/icons",
-    name: "Icons",
+    name: "Guidelines",
     icon: "ni ni-planet text-blue",
     component: Icons,
     layout: "/admin"
   },
   {
     path: "/maps",
-    name: "Maps",
+    name: "Reservations",
     icon: "ni ni-pin-3 text-orange",
     component: Maps,
     layout: "/admin"
   },
   {
     path: "/user-profile",
-    name: "User Profile",
+    name: "My Profile",
     icon: "ni ni-single-02 text-yellow",
     component: Profile,
     layout: "/admin"
   },
   {
     path: "/tables",
-    name: "Tables",
+    name: "Messages",
     icon: "ni ni-bullet-list-67 text-red",
     component: Tables,
     layout: "/admin"
   },
   {
     path: "/login",
-    name: "Login",
-    icon: "ni ni-key-25 text-info",
+    name: "Materials",
+    icon: "ni ni-collection-25 text-info",
     component: Login,
-    layout: "/auth"
-  },
-  {
-    path: "/register",
-    name: "Register",
-    icon: "ni ni-circle-08 text-pink",
-    component: Register,
     layout: "/auth"
   }
 ];
